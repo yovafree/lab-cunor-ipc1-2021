1d4821f4e528090b94ccf26e04f21b0d96798af7
Yova de León <erwingeo1021@gmail.com>
2021-03-20 11:52:51 -0600

    Ejemplo de registro de listado de estudiantes con Arrays.

--- JAVA/Ejemplo1/src/ejemplo1/Ejemplo10.java
+++ JAVA/Ejemplo1/src/ejemplo1/Ejemplo10.java
@@ -8,14 +8,21 @@ package ejemplo1;
 import java.util.Scanner;
 
 /**
- *
+ * Registrar un listado de estudiantes
  * @author erwin
  */
 public class Ejemplo10 {
     static int op = 0;
     
+    static int contador = 0;
+    
+    static String[] lstEstudiantes;
+    
     
     public static void main(String[] args){
+        lstEstudiantes = new String[100];
+        llenarEstudiantes();
+        
         do{
             menu();
             Scanner sc = new Scanner(System.in);
@@ -23,15 +30,45 @@ public class Ejemplo10 {
             
             switch(op){
                 case 1:
+                    System.out.println("\nIngrese el nombre del estudiante:");
+                    String nombre = sc.nextLine();
+                    
+                    lstEstudiantes[contador] = nombre;
+                    contador++;
                     break;
                 case 2:
+                    mostrarAlumnos();
+                    System.out.println("\nIngrese el no. de estudiante que desea editar:");
+                    int noAlumno = Integer.parseInt(sc.nextLine())-1;
+                    
+                    System.out.println("Ingrese el nombre del estudiante:");
+                    String estudiante = sc.nextLine();
+                    lstEstudiantes[noAlumno] = estudiante;
+                    
                     break;
                 case 3:
+                    mostrarAlumnos();
+                    sc.next();
                     break;
                 case 4:
+                    mostrarAlumnos();
+                    System.out.println("\nIngrese el no. de estudiante que desea eliminar:");
+                    noAlumno = Integer.parseInt(sc.nextLine())-1;
+                    
+                    lstEstudiantes[noAlumno] = "-----------";
                     break;
             }
         }while(op!= 7);
+    }
+    
+    public static void llenarEstudiantes(){
+        lstEstudiantes[contador++] = "Carlos Pan";
+        lstEstudiantes[contador++] = "Daniel San";
+        lstEstudiantes[contador++] = "Juan Pan";
+        lstEstudiantes[contador++] = "Alberto Pan";
+        lstEstudiantes[contador++] = "Marcos Pan";
+        lstEstudiantes[contador++] = "Luis Pan";
+        lstEstudiantes[contador++] = "Juana Pan";
     }
     
     public static void menu(){
@@ -45,6 +82,17 @@ public class Ejemplo10 {
         System.out.println("7) Salir");
         System.out.println("Ingrese la opción: ");
     }
+    
+    public static void mostrarAlumnos(){
+        System.out.println("\n-------------------");
+        System.out.println("  LISTADO DE ALUMNOS");
+        System.out.println("-------------------");
+
+        for (int x =0;x<=contador-1;x++){
+            System.out.println("" + (x+1) + ") " + lstEstudiantes[x]);
+        }
+    }
+    
     
     
 }
